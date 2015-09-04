c179fd29c206c0bf9fac1e2dfa43e4d71c4f5a7b
Skylar Moore <nickgray0@gmail.com>
2015-09-04 00:00:58 -0700

    Update EntryPoint.cpp

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -15,7 +15,7 @@ void mvfinestRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
 void mvflowblurRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
 //void mvflowinterRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
 //void mvflowfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-//void mvblockfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
+void mvblockfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
 //void mvscdetectionRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
 
 
@@ -24,7 +24,7 @@ VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegiste
 
     mvsuperRegister(registerFunc, plugin);
     mvanalyzeRegister(registerFunc, plugin);
-    mvanalyseRegister(registerFunc, plugin);
+	mvanalyseRegister(registerFunc, plugin);
     mvdegrainsRegister(registerFunc, plugin);
     mvcompensateRegister(registerFunc, plugin);
     mvrecalculateRegister(registerFunc, plugin);
@@ -33,6 +33,6 @@ VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegiste
     mvflowblurRegister(registerFunc, plugin);
     //mvflowinterRegister(registerFunc, plugin);
     //mvflowfpsRegister(registerFunc, plugin);
-    //mvblockfpsRegister(registerFunc, plugin);
+    mvblockfpsRegister(registerFunc, plugin);
     //mvscdetectionRegister(registerFunc, plugin);
 }
