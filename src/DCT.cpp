0abcf5d93740091af10050f785ddbcd8968c172b
Skylar Moore <nickgray0@gmail.com>
2016-01-21 05:44:23 -0800

    Update DCT.cpp

--- src/DCT.cpp
+++ src/DCT.cpp
@@ -1,12 +1,8 @@
 #include "DCT.h"
 
-DCTClass::DCTClass(int _sizex, int _sizey, int _dctmode):
-      sizex(_sizex)
-    , sizey(_sizey)
-    , dctmode(_dctmode)
-{
-}
+DCTClass::DCTClass(int _sizex, int _sizey, int _dctmode) :
+	sizex(_sizex)
+	, sizey(_sizey)
+	, dctmode(_dctmode) {}
 
-DCTClass::~DCTClass()
-{
-}
+DCTClass::~DCTClass() {}
