78145003f9dd568228466fb21684082cbb8a4599
Skylar Moore <nickgray0@gmail.com>
2016-01-21 06:21:41 -0800

    Update fftw3.h

--- src/fftw3.h
+++ src/fftw3.h
@@ -1,48 +1,48 @@
 /*
-* Copyright (c) 2003, 2007-14 Matteo Frigo
-* Copyright (c) 2003, 2007-14 Massachusetts Institute of Technology
-*
-* The following statement of license applies *only* to this header file,
-* and *not* to the other files distributed with FFTW or derived therefrom:
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-*
-* 1. Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
-* OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) 2003, 2007-14 Matteo Frigo
+ * Copyright (c) 2003, 2007-14 Massachusetts Institute of Technology
+ *
+ * The following statement of license applies *only* to this header file,
+ * and *not* to the other files distributed with FFTW or derived therefrom:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /***************************** NOTE TO USERS *********************************
-*
-*                 THIS IS A HEADER FILE, NOT A MANUAL
-*
-*    If you want to know how to use FFTW, please read the manual,
-*    online at http://www.fftw.org/doc/ and also included with FFTW.
-*    For a quick start, see the manual's tutorial section.
-*
-*   (Reading header files to learn how to use a library is a habit
-*    stemming from code lacking a proper manual.  Arguably, it's a
-*    *bad* habit in most cases, because header files can contain
-*    interfaces that are not part of the public, stable API.)
-*
-****************************************************************************/
+ *
+ *                 THIS IS A HEADER FILE, NOT A MANUAL
+ *
+ *    If you want to know how to use FFTW, please read the manual,
+ *    online at http://www.fftw.org/doc/ and also included with FFTW.
+ *    For a quick start, see the manual's tutorial section.
+ *
+ *   (Reading header files to learn how to use a library is a habit
+ *    stemming from code lacking a proper manual.  Arguably, it's a
+ *    *bad* habit in most cases, because header files can contain
+ *    interfaces that are not part of the public, stable API.)
+ *
+ ****************************************************************************/
 
 #ifndef FFTW3_H
 #define FFTW3_H
@@ -54,8 +54,8 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-	/* If <complex.h> is included, use the C99 complex type.  Otherwise
-	define a type bit-compatible with C99 complex */
+/* If <complex.h> is included, use the C99 complex type.  Otherwise
+   define a type bit-compatible with C99 complex */
 #if !defined(FFTW_NO_Complex) && defined(_Complex_I) && defined(complex) && defined(I)
 #  define FFTW_DEFINE_COMPLEX(R, C) typedef R _Complex C
 #else
@@ -68,15 +68,17 @@ extern "C"
 #define FFTW_MANGLE_LONG_DOUBLE(name) FFTW_CONCAT(fftwl_, name)
 #define FFTW_MANGLE_QUAD(name) FFTW_CONCAT(fftwq_, name)
 
-	/* IMPORTANT: for Windows compilers, you should add a line
-	#define FFTW_DLL
-	here and in kernel/ifftw.h if you are compiling/using FFTW as a
-	DLL, in order to do the proper importing/exporting, or
-	alternatively compile with -DFFTW_DLL or the equivalent
-	command-line flag.  This is not necessary under MinGW/Cygwin, where
-	libtool does the imports/exports automatically. */
+/* IMPORTANT: for Windows compilers, you should add a line
+*/
+#define FFTW_DLL
+/*
+   here and in kernel/ifftw.h if you are compiling/using FFTW as a
+   DLL, in order to do the proper importing/exporting, or
+   alternatively compile with -DFFTW_DLL or the equivalent
+   command-line flag.  This is not necessary under MinGW/Cygwin, where
+   libtool does the imports/exports automatically. */
 #if defined(FFTW_DLL) && (defined(_WIN32) || defined(__WIN32__))
-	/* annoying Windows syntax for shared-library declarations */
+   /* annoying Windows syntax for shared-library declarations */
 #  if defined(COMPILING_FFTW) /* defined in api.h when compiling FFTW */
 #    define FFTW_EXTERN extern __declspec(dllexport) 
 #  else /* user is calling FFTW; import symbol */
@@ -86,36 +88,36 @@ extern "C"
 #  define FFTW_EXTERN extern
 #endif
 
-	enum fftw_r2r_kind_do_not_use_me {
-		FFTW_R2HC = 0, FFTW_HC2R = 1, FFTW_DHT = 2,
-		FFTW_REDFT00 = 3, FFTW_REDFT01 = 4, FFTW_REDFT10 = 5, FFTW_REDFT11 = 6,
-		FFTW_RODFT00 = 7, FFTW_RODFT01 = 8, FFTW_RODFT10 = 9, FFTW_RODFT11 = 10
-	};
+enum fftw_r2r_kind_do_not_use_me {
+     FFTW_R2HC=0, FFTW_HC2R=1, FFTW_DHT=2,
+     FFTW_REDFT00=3, FFTW_REDFT01=4, FFTW_REDFT10=5, FFTW_REDFT11=6,
+     FFTW_RODFT00=7, FFTW_RODFT01=8, FFTW_RODFT10=9, FFTW_RODFT11=10
+};
 
-	struct fftw_iodim_do_not_use_me {
-		int n;                     /* dimension size */
-		int is;			/* input stride */
-		int os;			/* output stride */
-	};
+struct fftw_iodim_do_not_use_me {
+     int n;                     /* dimension size */
+     int is;			/* input stride */
+     int os;			/* output stride */
+};
 
 #include <stddef.h> /* for ptrdiff_t */
-	struct fftw_iodim64_do_not_use_me {
-		ptrdiff_t n;                     /* dimension size */
-		ptrdiff_t is;			/* input stride */
-		ptrdiff_t os;			/* output stride */
-	};
+struct fftw_iodim64_do_not_use_me {
+     ptrdiff_t n;                     /* dimension size */
+     ptrdiff_t is;			/* input stride */
+     ptrdiff_t os;			/* output stride */
+};
 
-	typedef void(*fftw_write_char_func_do_not_use_me)(char c, void *);
-	typedef int(*fftw_read_char_func_do_not_use_me)(void *);
+typedef void (*fftw_write_char_func_do_not_use_me)(char c, void *);
+typedef int (*fftw_read_char_func_do_not_use_me)(void *);
 
-	/*
-	huge second-order macro that defines prototypes for all API
-	functions.  We expand this macro for each supported precision
-
-	X: name-mangling macro
-	R: real data type
-	C: complex data type
-	*/
+/*
+  huge second-order macro that defines prototypes for all API
+  functions.  We expand this macro for each supported precision
+ 
+  X: name-mangling macro
+  R: real data type
+  C: complex data type
+*/
 
 #define FFTW_DEFINE_API(X, R, C)					   \
 									   \
@@ -130,8 +132,8 @@ typedef enum fftw_r2r_kind_do_not_use_me X(r2r_kind);			   \
 									   \
 typedef fftw_write_char_func_do_not_use_me X(write_char_func);		   \
 typedef fftw_read_char_func_do_not_use_me X(read_char_func);		   \
-                                                                           \
-FFTW_EXTERN void X(execute)(const X(plan) p);                              \
+									   \
+FFTW_EXTERN void X(execute)(const X(plan) p);				   \
 									   \
 FFTW_EXTERN X(plan) X(plan_dft)(int rank, const int *n,			   \
 		    C *in, C *out, int sign, unsigned flags);		   \
@@ -318,7 +320,6 @@ FFTW_EXTERN void X(set_timelimit)(double t);				   \
 FFTW_EXTERN void X(plan_with_nthreads)(int nthreads);			   \
 FFTW_EXTERN int X(init_threads)(void);					   \
 FFTW_EXTERN void X(cleanup_threads)(void);				   \
-FFTW_EXTERN void X(make_planner_thread_safe)(void);                        \
 									   \
 FFTW_EXTERN int X(export_wisdom_to_filename)(const char *filename);	   \
 FFTW_EXTERN void X(export_wisdom_to_file)(FILE *output_file);		   \
@@ -351,27 +352,27 @@ FFTW_EXTERN const char X(cc)[];						   \
 FFTW_EXTERN const char X(codelet_optim)[];
 
 
-	/* end of FFTW_DEFINE_API macro */
+/* end of FFTW_DEFINE_API macro */
 
-	FFTW_DEFINE_API(FFTW_MANGLE_DOUBLE, double, fftw_complex)
-		FFTW_DEFINE_API(FFTW_MANGLE_FLOAT, float, fftwf_complex)
-		FFTW_DEFINE_API(FFTW_MANGLE_LONG_DOUBLE, long double, fftwl_complex)
+FFTW_DEFINE_API(FFTW_MANGLE_DOUBLE, double, fftw_complex)
+FFTW_DEFINE_API(FFTW_MANGLE_FLOAT, float, fftwf_complex)
+FFTW_DEFINE_API(FFTW_MANGLE_LONG_DOUBLE, long double, fftwl_complex)
 
-		/* __float128 (quad precision) is a gcc extension on i386, x86_64, and ia64
-		for gcc >= 4.6 (compiled in FFTW with --enable-quad-precision) */
+/* __float128 (quad precision) is a gcc extension on i386, x86_64, and ia64
+   for gcc >= 4.6 (compiled in FFTW with --enable-quad-precision) */
 #if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)) \
- && !(defined(__ICC) || defined(__INTEL_COMPILER) || defined(__CUDACC__) || defined(__PGI)) \
+ && !(defined(__ICC) || defined(__INTEL_COMPILER)) \
  && (defined(__i386__) || defined(__x86_64__) || defined(__ia64__))
 #  if !defined(FFTW_NO_Complex) && defined(_Complex_I) && defined(complex) && defined(I)
-		/* note: __float128 is a typedef, which is not supported with the _Complex
-		keyword in gcc, so instead we use this ugly __attribute__ version.
-		However, we can't simply pass the __attribute__ version to
-		FFTW_DEFINE_API because the __attribute__ confuses gcc in pointer
-		types.  Hence redefining FFTW_DEFINE_COMPLEX.  Ugh. */
+/* note: __float128 is a typedef, which is not supported with the _Complex
+         keyword in gcc, so instead we use this ugly __attribute__ version.
+         However, we can't simply pass the __attribute__ version to
+         FFTW_DEFINE_API because the __attribute__ confuses gcc in pointer
+         types.  Hence redefining FFTW_DEFINE_COMPLEX.  Ugh. */
 #    undef FFTW_DEFINE_COMPLEX
 #    define FFTW_DEFINE_COMPLEX(R, C) typedef _Complex float __attribute__((mode(TC))) C
 #  endif
-		FFTW_DEFINE_API(FFTW_MANGLE_QUAD, __float128, fftwq_complex)
+FFTW_DEFINE_API(FFTW_MANGLE_QUAD, __float128, fftwq_complex)
 #endif
 
 #define FFTW_FORWARD (-1)
@@ -379,7 +380,7 @@ FFTW_EXTERN const char X(codelet_optim)[];
 
 #define FFTW_NO_TIMELIMIT (-1.0)
 
-		/* documented flags */
+/* documented flags */
 #define FFTW_MEASURE (0U)
 #define FFTW_DESTROY_INPUT (1U << 0)
 #define FFTW_UNALIGNED (1U << 1)
@@ -390,7 +391,7 @@ FFTW_EXTERN const char X(codelet_optim)[];
 #define FFTW_ESTIMATE (1U << 6)
 #define FFTW_WISDOM_ONLY (1U << 21)
 
-		/* undocumented beyond-guru flags */
+/* undocumented beyond-guru flags */
 #define FFTW_ESTIMATE_PATIENT (1U << 7)
 #define FFTW_BELIEVE_PCOST (1U << 8)
 #define FFTW_NO_DFT_R2HC (1U << 9)
@@ -411,4 +412,3 @@ FFTW_EXTERN const char X(codelet_optim)[];
 #endif /* __cplusplus */
 
 #endif /* FFTW3_H */
-
