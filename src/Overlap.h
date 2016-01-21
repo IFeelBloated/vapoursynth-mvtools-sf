30cb49089767919ee70ce39174022924941c96a7
Skylar Moore <nickgray0@gmail.com>
2016-01-21 06:14:19 -0800

    Update Overlap.h

--- src/Overlap.h
+++ src/Overlap.h
@@ -1,8 +1,6 @@
 #ifndef __OVERLAP__
 #define __OVERLAP__
 
-#include <math.h>
-#include <stdint.h>
 #ifndef M_PI
 #define M_PI       3.14159265358979323846f
 #endif
@@ -15,7 +13,6 @@
 #define max(a,b)            (((a) < (b)) ? (b) : (a))
 #endif
 
-// top, middle, botom and left, middle, right windows
 #define OW_TL 0
 #define OW_TM 1
 #define OW_TR 2
@@ -26,79 +23,65 @@
 #define OW_BM 7
 #define OW_BR 8
 
-class OverlapWindows
-{
-    int nx; // window sizes
-    int ny;
-    int ox; // overap sizes
-    int oy;
-    int size; // full window size= nx*ny
+#include <cmath>
+#include <cstdint>
 
-    short * Overlap9Windows;
-
-    float *fWin1UVx;
-    float *fWin1UVxfirst;
-    float *fWin1UVxlast;
-    float *fWin1UVy;
-    float *fWin1UVyfirst;
-    float *fWin1UVylast;
-    public :
-
-    OverlapWindows(int _nx, int _ny, int _ox, int _oy);
-    ~OverlapWindows();
-
-    inline int Getnx() const { return nx; }
-    inline int Getny() const { return ny; }
-    inline int GetSize() const { return size; }
-    inline short *GetWindow(int i) const { return Overlap9Windows + size*i; }
+class OverlapWindows {
+	int nx;
+	int ny;
+	int ox;
+	int oy;
+	int size;
+	short * Overlap9Windows;
+	float *fWin1UVx;
+	float *fWin1UVxfirst;
+	float *fWin1UVxlast;
+	float *fWin1UVy;
+	float *fWin1UVyfirst;
+	float *fWin1UVylast;
+public:
+	OverlapWindows(int _nx, int _ny, int _ox, int _oy);
+	~OverlapWindows();
+	inline int Getnx() const { return nx; }
+	inline int Getny() const { return ny; }
+	inline int GetSize() const { return size; }
+	inline short *GetWindow(int i) const { return Overlap9Windows + size*i; }
 };
 
-typedef void (*OverlapsFunction)(uint8_t *pDst, intptr_t nDstPitch,
-        const uint8_t *pSrc, intptr_t nSrcPitch,
-        short *pWin, intptr_t nWinPitch);
-
+typedef void(*OverlapsFunction)(uint8_t *pDst, intptr_t nDstPitch,
+	const uint8_t *pSrc, intptr_t nSrcPitch,
+	short *pWin, intptr_t nWinPitch);
 
 template <int blockWidth, int blockHeight, typename PixelType2, typename PixelType>
-void Overlaps_C(uint8_t *pDst8, intptr_t nDstPitch, const uint8_t *pSrc8, intptr_t nSrcPitch, short *pWin, intptr_t nWinPitch)
-{
-    // pWin from 0 to 2048
-    for (int j=0; j<blockHeight; j++)
-    {
-        for (int i=0; i<blockWidth; i++)
-        {
-            PixelType2 *pDst = (PixelType2 *)pDst8;
-            const PixelType *pSrc = (const PixelType *)pSrc8;
-
-            pDst[i] += ((pSrc[i] * pWin[i]) / 64);
-        }
-        pDst8 += nDstPitch;
-        pSrc8 += nSrcPitch;
-        pWin += nWinPitch;
-    }
+void Overlaps_C(uint8_t *pDst8, intptr_t nDstPitch, const uint8_t *pSrc8, intptr_t nSrcPitch, short *pWin, intptr_t nWinPitch) {
+	for (int j = 0; j<blockHeight; j++) {
+		for (int i = 0; i<blockWidth; i++) {
+			PixelType2 *pDst = (PixelType2 *)pDst8;
+			const PixelType *pSrc = (const PixelType *)pSrc8;
+			pDst[i] += ((pSrc[i] * pWin[i]) / 64);
+		}
+		pDst8 += nDstPitch;
+		pSrc8 += nSrcPitch;
+		pWin += nWinPitch;
+	}
 }
 
-typedef void (*ToPixelsFunction)(uint8_t *pDst, int nDstPitch,
-        const uint8_t *pSrc, int nSrcPitch,
-        int width, int height);
-
+typedef void(*ToPixelsFunction)(uint8_t *pDst, int nDstPitch,
+	const uint8_t *pSrc, int nSrcPitch,
+	int width, int height);
 
 template <typename PixelType2, typename PixelType>
-void ToPixels(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, int nSrcPitch, int nWidth, int nHeight)
-{
-
-    for (int h=0; h<nHeight; h++)
-    {
-        for (int i=0; i<nWidth; i++)
-        {
-            const PixelType2 *pSrc = (const PixelType2 *)pSrc8;
-            PixelType *pDst = (PixelType *)pDst8;
-
-            float a = float (pSrc[i] / 32);
-                pDst[i] = min(1.f, a);
-        }
-        pDst8 += nDstPitch;
-        pSrc8 += nSrcPitch;
-    }
+void ToPixels(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, int nSrcPitch, int nWidth, int nHeight) {
+	for (int h = 0; h<nHeight; h++) {
+		for (int i = 0; i<nWidth; i++) {
+			const PixelType2 *pSrc = (const PixelType2 *)pSrc8;
+			PixelType *pDst = (PixelType *)pDst8;
+			float a = float(pSrc[i] / 32);
+			pDst[i] = min(1.f, a);
+		}
+		pDst8 += nDstPitch;
+		pSrc8 += nSrcPitch;
+	}
 }
 
 #endif
