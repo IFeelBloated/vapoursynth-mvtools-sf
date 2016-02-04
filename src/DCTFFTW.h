b3e44b4ebe3699df13d170bf06079fbba860d7e5
Skylar Moore <nickgray0@gmail.com>
2016-02-04 03:51:24 -0800

    Update DCTFFTW.h

--- src/DCTFFTW.h
+++ src/DCTFFTW.h
@@ -4,15 +4,15 @@
 #include "DCT.h"
 
 class DCTFFTW : public DCTClass {
-	float * fSrc;
-	fftwf_plan dctplan;
-	float * fSrcDCT;
+	double *fSrc;
+	fftw_plan dctplan;
+	double *fSrcDCT;
 	int dctshift;
 	int dctshift0;
 	template <typename PixelType>
-	void Bytes2Float(const uint8_t * srcp0, int _pitch, float * realdata);
+	void Bytes2Float(const uint8_t *srcp0, int _pitch, double *realdata);
 	template <typename PixelType>
-	void Float2Bytes(uint8_t * srcp0, int _pitch, float * realdata);
+	void Float2Bytes(uint8_t *srcp0, int _pitch, double *realdata);
 public:
 	DCTFFTW(int _sizex, int _sizey, int _dctmode);
 	~DCTFFTW();
